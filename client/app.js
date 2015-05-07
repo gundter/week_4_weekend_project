b747e3e87c5850370363a27b5b4febd255fb7c85
Terry Gunderson <tgunderson25@gmail.com>
2015-05-07 18:28:35 -0500

    implents the slide toggle effect for the show more buttons, adds jumbroton bootstrap to header

--- client/app.js
+++ client/app.js
@@ -2,21 +2,21 @@ $(document).ready(function(){
    $('.block-content').hide();
 
     $('.wellsRelease').on('click', '.btn', function() {
-        $(this).prev().slideDown('slow');
+        $(this).prev().slideToggle('slow');
     });
     $('.wellsVault').on('click', '.btn', function() {
-        $(this).prev().slideDown('slow');
+        $(this).prev().slideToggle('slow');
     });
     $('.wellsVaultClerk').on('click', '.btn', function() {
-        $(this).prev().slideDown('slow');
+        $(this).prev().slideToggle('slow');
     });
     $('.wellsData').on('click', '.btn', function() {
-        $(this).prev().slideDown('slow');
+        $(this).prev().slideToggle('slow');
     });
     $('.education').on('click', '.btn', function() {
-        $(this).prev().slideDown('slow');
+        $(this).prev().slideToggle('slow');
     });
     $('.skills').on('click', '.btn', function() {
-        $(this).prev().slideDown('slow');
+        $(this).prev().slideToggle('slow');
     });
 });
